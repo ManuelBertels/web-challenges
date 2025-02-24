8b1a4de78b384d51972299f61ec64d13c6d0ac2f
Manuel <argument689@gmail.com>
2025-02-24 11:10:55 +0100

    neu

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,15 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const data = await fetch(url);
+
+  const dataobj = await data.json();
+
+  console.log(dataobj);
+
+  for (let i = 0; i <= dataobj.results.length; i++)
+    console.log(dataobj.results[i]);
+}
 
 fetchData();
