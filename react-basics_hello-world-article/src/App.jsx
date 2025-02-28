3c50c34778306a160f6e2d8813154934b1beaf01
Manuel <argument689@gmail.com>
2025-02-28 12:55:47 +0100

    neu

---
+++ react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorlsArticle />;
+}
+
+function HelloWorlsArticle() {
+  return (
+    <>
+      <h3>Hallo, das ist mein Artikel</h3>
+      <p>
+        Ich lerne aktuell React und bin großer <br></br>Zuversicht das alles
+        funktioneren wird.
+      </p>
+    </>
+  );
+}
